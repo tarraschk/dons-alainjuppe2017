e22c32a4f450dbb8b5d4dde9e83247eeef2a434e
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-07-03 11:59:35 +0200

    activation SSL forcée

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module App
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    #config.force_ssl = false
-    config.force_ssl = (ENV['ssl_enable'] == 1)
+    config.force_ssl = true
   end
 end
